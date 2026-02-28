f5231eb14e68493765f3e799c85bb87b55923f50
Duna Majhi <dunamajhi894@gmail.com>
2026-02-28 17:48:42 +0530

    Allow external images from Unsplash in next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,18 @@ const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
   allowedDevOrigins: ['*', "localhost:3000", "trycloudflare.com", "*.trycloudflare.com"],
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
+      },
+      {
+        protocol: 'https',
+        hostname: '*.supabase.co',
+      }
+    ],
+  },
 };
 
 export default nextConfig;
